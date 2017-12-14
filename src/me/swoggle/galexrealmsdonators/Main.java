d8a94f363dd0417577109af3cb0b93b1789ce803
Swoggle.J <jefftilted@gmail.com>
2017-12-14 16:42:41 +0000

    Initial commit

---
+++ src/me/swoggle/galexrealmsdonators/Main.java
@@ -0,0 +1,20 @@
+package me.swoggle.galexrealmsdonators;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+
+    }
+
+    @Override
+    public void onDisable() {
+
+
+    }
+
+}
+
